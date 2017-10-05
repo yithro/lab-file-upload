e3c8a77bac3cf3daaa0af5ec691541864b8666be
yithro <dav.pinhas@gmail.com>
2017-10-05 15:31:40 +0200

    second

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,7 @@ const MongoStore         = require('connect-mongo')(session);
 const mongoose           = require('mongoose');
 const flash              = require('connect-flash');
 
+
 mongoose.connect('mongodb://localhost:27017/tumblr-lab-development');
 
 const app = express();
